2e8ae3aae464d6716fd06356447da10b5c967a09
Daniele Favara <daniele.favara@retex.com>
2026-07-18 21:26:58 +0200

    feat: export complete reconciliation API

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./contract.js";
 export * from "./mutation.js";
 export * from "./policy.js";
 export * from "./project.js";
+export * from "./reconcile.js";
 export * from "./relationship-application.js";
 export * from "./relationships.js";
 export * from "./report.js";
