d62f52f035b8e88d6f376846a1aae3f5537d81df
Daniele Favara <daniele.favara@retex.com>
2026-07-18 21:14:39 +0200

    chore: export relationship application API

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./contract.js";
 export * from "./mutation.js";
 export * from "./policy.js";
 export * from "./project.js";
+export * from "./relationship-application.js";
 export * from "./relationships.js";
 export * from "./report.js";
